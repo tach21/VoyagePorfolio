23486f916f50262d0c73b0cb15a32da9dc6523c7
Tacha <degiovanni21tach@gmail.com>
2021-02-03 22:32:44 +0100

    creaction of faq section

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAG</span>
+      </h2>
+      <div className="question">
+        <h4>How to order</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          <p>
+            Vero natus dolores illo quod! Aperiam, et! Similique odio accusamus
+            fugiat quidem perferendis nulla, natus vero ad explicabo? Placeat
+            quasi saepe harum?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Shipping</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          <p>
+            Vero natus dolores illo quod! Aperiam, et! Similique odio accusamus
+            fugiat quidem perferendis nulla, natus vero ad explicabo? Placeat
+            quasi saepe harum?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Creations in demand</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          <p>
+            Vero natus dolores illo quod! Aperiam, et! Similique odio accusamus
+            fugiat quidem perferendis nulla, natus vero ad explicabo? Placeat
+            quasi saepe harum?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Ordering a unique design</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          <p>
+            Vero natus dolores illo quod! Aperiam, et! Similique odio accusamus
+            fugiat quidem perferendis nulla, natus vero ad explicabo? Placeat
+            quasi saepe harum?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/BioPage.jsx
+++ src/pages/BioPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Bio from "../components/Bio";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const BioPage = () => {
   return (
     <>
       <Bio />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
